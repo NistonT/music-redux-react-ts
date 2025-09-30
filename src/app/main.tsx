2fb22e2eb057364dd82494632d2525206bac9785
NistonN <nisprogrammer@gmail.com>
2025-09-30 18:08:28 +0300

    first commit

---
+++ src/app/main.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ReduxProvider } from "./providers/ReduxProvider";
+
+createRoot(document.getElementById("root")!).render(
+  <ReduxProvider>
+    <App />
+  </ReduxProvider>,
+);
