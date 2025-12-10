20ed964dcd1c2db72eb9a4b23e33a252b2c9eea0
NistonN <nisprogrammer@gmail.com>
2025-12-11 01:00:32 +0300

    [XXXX] open/close left bar

--- src/app/layout/NavigationLayout.tsx
+++ src/app/layout/NavigationLayout.tsx
@@ -1,10 +1,26 @@
+import { useLeftBar } from "@/entities/leftBar/hook/useLeftBar";
+import { ButtonIconCustom } from "@/shared/ui";
 import { LeftBar } from "@/widgets";
+import { ArrowRight } from "lucide-react";
 import { PropsWithChildren } from "react";
 export const NavigationLayout = ({ children }: PropsWithChildren) => {
+  const { leftBar, handleOpenLeftBar } = useLeftBar();
+
   return (
     <>
-      <LeftBar />
-      <div>{children}</div>
+      {leftBar ? (
+        <div className="flex w-full">
+          <LeftBar />
+          <div className="w-5/6">{children}</div>
+        </div>
+      ) : (
+        <div>
+          <ButtonIconCustom onClick={handleOpenLeftBar} type={"button"} className="absolute left-0 top-0 text-main-pink">
+            <ArrowRight />
+          </ButtonIconCustom>
+          {children}
+        </div>
+      )}
     </>
   );
 };
