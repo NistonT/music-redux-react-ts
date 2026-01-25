b4e54d38feb558ba72e0f7c2fd7a1fc073e7edb7
NistonN <nisprogrammer@gmail.com>
2026-01-25 20:02:28 +0300

    [0001] create main page

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,9 +1,11 @@
+import { MainPage } from "@/pages/MainPage";
+import { pageRouter } from "@/shared/constants/page-router";
 import { Route, Routes } from "react-router";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<div>Hello world</div>} />
+      <Route path={pageRouter.Home} element={<MainPage />} />
     </Routes>
   );
 };
