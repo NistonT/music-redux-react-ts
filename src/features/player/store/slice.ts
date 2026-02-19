259c59b340db15f545746d4f86126f1490626ed8
NistonN <nisprogrammer@gmail.com>
2026-02-19 15:48:51 +0300

    [0008] add changeTrack

--- src/features/player/store/slice.ts
+++ src/features/player/store/slice.ts
@@ -1,3 +1,4 @@
+import { tracks } from "@/shared/constants/tracks";
 import type { ITrack } from "@/shared/model/types";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
@@ -45,8 +46,21 @@ export const playerSlice = createSlice({
     setVolume(state, action: PayloadAction<number>) {
       state.volume = action.payload;
     },
+
+    changeTrack(state, action: PayloadAction<{ type: "next" | "prev"; playlistId?: number; likes?: number; genres?: number }>) {
+      if (!state.currentTrack) return;
+
+      const currentIndex = tracks.findIndex((t) => t.name === state.currentTrack?.name);
+
+      const nextIndex = action.payload.type === "next" ? (currentIndex + 1) % tracks.length : (currentIndex - 1 + tracks.length) % tracks.length;
+
+      setTrack({ track: tracks[nextIndex] });
+
+      state.progress = 0;
+      state.currentTime = 0;
+    },
   },
 });
 
-export const { setTrack, play, stop, seek, togglePlayPause, setVolume } = playerSlice.actions;
+export const { setTrack, play, stop, seek, togglePlayPause, setVolume, changeTrack } = playerSlice.actions;
 export default playerSlice.reducer;
