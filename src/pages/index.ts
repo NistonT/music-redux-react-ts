ef3a7d60a4fb0d0a02b59424e678ec4afc80b81b
NistonN <nisprogrammer@gmail.com>
2026-02-28 21:31:59 +0300

    [0015] add lazy suspence

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,5 @@
-import { AuthorIdPage } from "./AuthorIdPage";
 import { AuthorsPage } from "./AuthorsPage";
 import { LibraryPage } from "./LibraryPage";
 import { MainPage } from "./MainPage";
 import { SearchPage } from "./SearchPage";
-export { AuthorIdPage, AuthorsPage, LibraryPage, MainPage, SearchPage };
+export { AuthorsPage, LibraryPage, MainPage, SearchPage };
