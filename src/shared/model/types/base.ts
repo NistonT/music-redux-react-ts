4f7eb9bc2637c55ae9206d8d45fd653a34d3bb14
NistonN <nisprogrammer@gmail.com>
2026-03-01 23:54:50 +0300

    [0016] add type genres

--- src/shared/model/types/base.ts
+++ src/shared/model/types/base.ts
@@ -1,3 +1,5 @@
 export type TypeNextPrev = "next" | "prev";
 
 export type TypeNotFound = "author" | "page";
+
+export type TypeGenresTrack = "pop" | "rock" | "electronic" | "ambient" | "indie" | "hiphop" | "other";
--- src/shared/model/types/track.ts
+++ src/shared/model/types/track.ts
@@ -3,10 +3,13 @@
 // dayjs.extend(unix)
 // dayjs.unix(track.duration).utc().format("m:ss")
 
+import type { TypeGenresTrack } from "./base";
+
 export interface ITrack {
   id: number;
   img: string;
   name: string;
   author: number;
   file: string;
+  genres?: TypeGenresTrack[];
 }
