140964b4432ed5c399080b1130afc3acb5e10b6a
NistonN <nisprogrammer@gmail.com>
2026-05-17 22:50:50 +0300

    [0020] add ico

--- src/widgets/LeftBar/index.tsx
+++ src/widgets/LeftBar/index.tsx
@@ -3,12 +3,12 @@ import { Navigators } from "./ui";
 export const LeftBar = ({ children }: PropsWithChildren) => {
   return (
     <div className="flex mx-auto w-full bg-bg">
-      <div className="w-2/12 p-4">
+      <div className="fixed left-0 top-0 w-2/12 h-full p-4 bg-bg z-10">
         <div className="mt-8">
           <Navigators />
         </div>
       </div>
-      <div className="w-10/12 text-white">{children}</div>
+      <div className="w-10/12 text-white ml-[16.66%]">{children}</div>
     </div>
   );
 };
